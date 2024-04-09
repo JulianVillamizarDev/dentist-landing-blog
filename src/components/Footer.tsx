2f94da075f508d715d03b8eaccf86e117d94bd19
Julian Villamizar <julianvillamizardev@gmail.com>
2024-04-08 20:45:46 -0500

    footer updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,31 +4,16 @@ import { navbarData,socialLinks } from "../utils/data";
 export default function Footer() {
     return (
         <footer >
-            <div className="max-w-7xl mx-auto p-6 mt-4 grid grid-cols-2 gap-4">
+            <div className="max-w-7xl mx-auto p-6 mt-4 flex flex-col items-center justify-center gap-4">
                 {/* logo */}
                 <div className="flex gap-2">
-                    <Image src="/logo-2.webp" className="w-8 h-8" alt="logo" />
-                    <span>Angela Villamizar</span>
+                    <Image src="/logo.webp" className="w-24 h-2w-24" alt="logo" />
                 </div>
                 {/* items */}
-                <div className="grid lg:grid-cols-2 gap-4 lg:gap-0">
-                    {/* nav items */}
-                    <div className="flex flex-col gap-2">
-                        <h1 className="text-lg font-medium">Paginas</h1>
-                        <ul>
-                            {
-                                navbarData.map((item) => (
-                                    <li key={item.title}>
-                                        <Link className="font-normal text-gray-400 hover:text-primary-400 w-full" href={item.href}>{item.title}</Link>
-                                    </li>
-                                ))
-                            }    
-                        </ul>
-                    </div>  
+                <div className="">
                     {/* socials items */}
-                    <div className="flex flex-col gap-2">
-                        <h1 className="text-lg font-medium">Encuentranos</h1>
-                        <ul>
+                    <div className="flex flex-col items-center justify-center gap-2">
+                        <ul className="flex flex-wrap gap-4 ">
                             {
                                 socialLinks.map((item) => (
                                     <li key={item.title}>
@@ -38,13 +23,13 @@ export default function Footer() {
                             }    
                         </ul>
                     </div>
-                    
-                    
                 </div>
                 {/* copy */}
-                <div className="col-span-2 py-4 flex items-center justify-center border-t border-gray-200">
-                    <p className="text-gray-400 text-sm"> &copy; {new Date().getFullYear()} Angela Villamizar</p>
-                </div>  
+                <p className="text-center text-gray-400 text-sm border-t border-gray-300 w-full py-4">
+                    Cúcuta, Norte de Santander, Colombia <br />
+                    Av. Libertadores, bahía Torres del Parque, Local C1 <br />
+                    <span className="text-xs"> &copy; {new Date().getFullYear()} Angela Villamizar </span> 
+                </p>
             </div>
         </footer>
     )
