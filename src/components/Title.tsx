3e0a159a262dceb74c63dae86e73fe9e1c36e537
Julian Villamizar <julianvillamizardev@gmail.com>
2024-05-02 12:11:33 -0500

    Title section fixed

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,13 +2,13 @@ import ToothIcon from "./icons/Tooth"
 export default function Title({ children, className = '', color = 'primary' }: any) {
     return (
         <div className={`flex flex-col items-center justify-center my-8 text-${color}-400 ${className}`}>
-            <h1 className="font-bold">
+            <h1 className="font-bold text-center">
                 {children}
             </h1>
             <footer className={`text-${color}-400 flex items-center justify-center gap-4 w-full mt-2`}>
-                <div className={`flex-1 h-1 w-full ${ color === "white"? "bg-white":`bg-${color}-400`} mt-2`}/>
+                <div className={`flex-1 h-1 w-full ${ color === "white"? "bg-white":"bg-"+color+"-400"} mt-2`}/>
                 <ToothIcon/>
-                <div className={`flex-1 h-1 w-full ${ color === "white"? "bg-white":`bg-${color}-400`} mt-2`}/>
+                <div className={`flex-1 h-1 w-full ${ color === "white"? "bg-white":"bg-"+color+"-400"} mt-2`}/>
             </footer>     
         </div>
     )
