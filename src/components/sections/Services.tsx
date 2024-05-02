c13df0708b8df28e5c496ddce82008874ac3210d
Julian Villamizar <julianvillamizardev@gmail.com>
2024-05-02 17:54:47 -0500

    Services section bg color changed to primary-400

--- src/components/sections/Services.tsx
+++ src/components/sections/Services.tsx
@@ -5,7 +5,7 @@ import Title from "../Title"
 
 export default function Services() {
     return (
-        <div className="bg-primary-300 text-white">
+        <div className="bg-primary-400 text-white">
             <Section id="services" className="flex flex-col items-center justify-center py-32">
                 <Title color="white">Conoce Nuestros Servicios</Title>
                 <div className="mt-8 flex flex-wrap gap-8 justify-center lg:justify-start">
